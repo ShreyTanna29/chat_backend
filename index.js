aec2b4c133a675d5ea7fdc359764c5d8dbe7ed2b
ShreyTanna29 <shreytanna2957@gmail.com>
2026-02-21 12:35:44 +0530

    increase payload size

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const server = http.createServer(app);
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Database connection
 prisma
